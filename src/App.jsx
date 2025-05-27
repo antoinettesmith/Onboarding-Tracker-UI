0420da1b22456c04c8414bc07d395131ec9ec883
Anniebsmith <149444116+Anniebsmith@users.noreply.github.com>
2025-05-27 09:36:03 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import OnboardingProgressTracker from "./OnboardingProgressTracker";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <OnboardingProgressTracker />
+    </div>
+  );
+}
